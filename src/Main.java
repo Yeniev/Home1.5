5f164f4c5036ee1f2085a4a4051697fc3c974d7e
a1 <Skif.Olshan@gmail.com>
2022-07-27 10:23:58 +0200

    переделал

--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,45 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Выводим суммы ");
         int[] array = new int[30];
+        int sum = array[0];
+        int max = array[0];
+        //  int min = array[0];
+        double mean = array[0];
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 100_000) + 100_000);
-            System.out.print(array[i] + ", ");
+            array[i] = (int) (Math.random() * 100_000) + 100_000;
+            sum += array[i];
         }
+        System.out.print(Arrays.toString(array));
         System.out.println();
-        System.out.println();
-        System.out.println("Задание 1");
-
-        int myArray[] = array;
-        int sum = 0;
-        for (int i = 0; i < myArray.length; i++) {
-            sum = sum + myArray[i];
+        System.out.println("Cумма трат за месяц составила " + sum + " рублей");
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
         }
-        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
-
-        System.out.println();
-        System.out.println("Задание 2");
-
-        int[] A = array;
-
-        int max = Arrays.stream(A)
-                .max()
-                .getAsInt();
-
-        int min = Arrays.stream(A)
-                .min()
-                .getAsInt();
-
-        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
         System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+        int min = max;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        for (int j : array) {
+            mean += j;
+        }
+        System.out.println("Средняя сумма трат за месяц " + (int) Math.abs(mean / 30));
+
 
         System.out.println();
-        System.out.println("Задание 3");
 
-        int[] B = array;
-
-        double Average = Arrays.stream(B)
-                .average()
-                .getAsDouble();
-        System.out.println("Средняя сумма трат за месяц составила " + Average + " рублей");
-
-        System.out.println();
-        System.out.println("Задане 4");
 
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        int i = reverseFullName.length - 1, j = 0;
-        char[] let = new char[reverseFullName.length];
-        while (i >= 0) {
-            let[j] = reverseFullName[i];
-            i--;
-            j++;
-        }
-        for (char c : let) {
-            System.out.print(c);
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(reverseFullName[i]);
+            } else System.out.print(reverseFullName[i]);
         }
     }
 }
